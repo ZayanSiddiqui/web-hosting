6509d7438295f5685cba0b82212a5fb7bcb70fcc
sameeriqbalsiddiqui09 <sameeriqbal00093@gmail.com>
2025-01-24 18:18:21 +0500

    we are learning about animals

---
+++ script.js
@@ -0,0 +1,22 @@
+// Scroll Animation effect
+const sections = document.querySelectorAll('section');
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
